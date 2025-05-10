d5969d1d6d0e3a7b4d4e5c5a29e1c8c7b32f945d
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2025-05-11 00:30:06 +0200

    serv client uptade start files + random

--- Example_Application/Game_Educative_Application/client.c
+++ Example_Application/Game_Educative_Application/client.c
@@ -2,9 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+#include <netdb.h>
+#include <arpa/inet.h>
+
+struct _client
+{
+        char ipAddress[40];
+        int port;
+        char name[40];
+} tcpClients[4];
+int nbClients;
+int fsmServer;
 int deck[13]={0,1,2,3,4,5,6,7,8,9,10,11,12};
 int tableCartes[4][8];
+char *nomcartes[]=
+{"Sebastian Moran", "irene Adler", "inspector Lestrade",
+  "inspector Gregson", "inspector Baynes", "inspector Bradstreet",
+  "inspector Hopkins", "Sherlock Holmes", "John Watson", "Mycroft Holmes",
+  "Mrs. Hudson", "Mary Morstan", "James Moriarty"};
+int joueurCourant;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
 
 void melangerDeck()
 {
@@ -111,7 +137,7 @@ void printDeck()
         int i,j;
 
         for (i=0;i<13;i++)
-                printf("%d:%d\n",i,deck[i]);
+                printf("%d %s\n",deck[i],nomcartes[deck[i]]);
 
 	for (i=0;i<4;i++)
 	{
@@ -121,12 +147,210 @@ void printDeck()
 	}
 }
 
+void printClients()
+{
+        int i;
+
+        for (i=0;i<nbClients;i++)
+                printf("%d: %s %5.5d %s\n",i,tcpClients[i].ipAddress,
+                        tcpClients[i].port,
+                        tcpClients[i].name);
+}
+
+int findClientByName(char *name)
+{
+        int i;
+
+        for (i=0;i<nbClients;i++)
+                if (strcmp(tcpClients[i].name,name)==0)
+                        return i;
+        return -1;
+}
+
+void sendMessageToClient(char *clientip,int clientport,char *mess)
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    server = gethostbyname(clientip);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(clientport);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        {
+                printf("ERROR connecting\n");
+                exit(1);
+        }
+
+        sprintf(buffer,"%s\n",mess);
+        n = write(sockfd,buffer,strlen(buffer));
+
+    close(sockfd);
+}
+
+void broadcastMessage(char *mess)
+{
+        int i;
+
+        for (i=0;i<nbClients;i++)
+                sendMessageToClient(tcpClients[i].ipAddress,
+                        tcpClients[i].port,
+                        mess);
+}
+
 int main(int argc, char *argv[])
 {
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+	int i;
+
+        char com;
+        char clientIpAddress[256], clientName[256];
+        int clientPort;
+        int id;
+        char reply[256];
+
+
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+
 	printDeck();
 	melangerDeck();
 	createTable();
 	printDeck();
+	joueurCourant=0;
 
+	for (i=0;i<4;i++)
+	{
+        	strcpy(tcpClients[i].ipAddress,"localhost");
+        	tcpClients[i].port=-1;
+        	strcpy(tcpClients[i].name,"-");
+	}
+
+     while (1)
+     {    
+     	newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     	if (newsockfd < 0) 
+          	error("ERROR on accept");
+
+     	bzero(buffer,256);
+     	n = read(newsockfd,buffer,255);
+     	if (n < 0) 
+		error("ERROR reading from socket");
+
+        printf("Received packet from %s:%d\nData: [%s]\n\n",
+                inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port), buffer);
+
+        if (fsmServer==0)
+        {
+        	switch (buffer[0])
+        	{
+                	case 'C':
+                        	sscanf(buffer,"%c %s %d %s", &com, clientIpAddress, &clientPort, clientName);
+                        	printf("COM=%c ipAddress=%s port=%d name=%s\n",com, clientIpAddress, clientPort, clientName);
+
+                        	// fsmServer==0 alors j'attends les connexions de tous les joueurs
+                                strcpy(tcpClients[nbClients].ipAddress,clientIpAddress);
+                                tcpClients[nbClients].port=clientPort;
+                                strcpy(tcpClients[nbClients].name,clientName);
+                                nbClients++;
+
+                                printClients();
+
+				// rechercher l'id du joueur qui vient de se connecter
+
+                                id=findClientByName(clientName);
+                                printf("id=%d\n",id);
+
+				// lui envoyer un message personnel pour lui communiquer son id
+
+                                sprintf(reply,"I %d",id);
+                                sendMessageToClient(tcpClients[id].ipAddress,
+                                       tcpClients[id].port,
+                                       reply);
+
+				// Envoyer un message broadcast pour communiquer a tout le monde la liste des joueurs actuellement
+				// connectes
+
+                                sprintf(reply,"L %s %s %s %s", tcpClients[0].name, tcpClients[1].name, tcpClients[2].name, tcpClients[3].name);
+                                broadcastMessage(reply);
+
+				// Si le nombre de joueurs atteint 4, alors on peut lancer le jeu
+
+                                if (nbClients==4)
+				{
+					// On envoie ses cartes au joueur 0, ainsi que la ligne qui lui correspond dans tableCartes
+					// RAJOUTER DU CODE ICI
+
+					// On envoie ses cartes au joueur 1, ainsi que la ligne qui lui correspond dans tableCartes
+					// RAJOUTER DU CODE ICI
+
+					// On envoie ses cartes au joueur 2, ainsi que la ligne qui lui correspond dans tableCartes
+					// RAJOUTER DU CODE ICI
+
+					// On envoie ses cartes au joueur 3, ainsi que la ligne qui lui correspond dans tableCartes
+					// RAJOUTER DU CODE ICI
+
+					// On envoie enfin un message a tout le monde pour definir qui est le joueur courant=0
+					// RAJOUTER DU CODE ICI
+
+                                        fsmServer=1;
+				}
+				break;
+                }
+	}
+	else if (fsmServer==1)
+	{
+		switch (buffer[0])
+		{
+                	case 'G':
+				// RAJOUTER DU CODE ICI
+				break;
+                	case 'O':
+				// RAJOUTER DU CODE ICI
+				break;
+			case 'S':
+				// RAJOUTER DU CODE ICI
+				break;
+                	default:
+                        	break;
+		}
+        }
+     	close(newsockfd);
+     }
+     close(sockfd);
      return 0; 
 }
+
+// With the help mentioned in the README and the code base provided in the instructions
